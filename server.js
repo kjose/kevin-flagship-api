ddc2c9ad94d5c65916590450854ef4f73c42953a
Kévin JOSE <kevin.jose.92@gmail.com>
2021-12-13 17:09:25 +0100

    add more fields

--- server.js
+++ server.js
@@ -9,31 +9,42 @@ app.use(cors());
 app.get('/api', (req, res) => {
     console.log("Call FS...");
     const vid = req.query.vid;
+    const apikey = req.query.apikey;
+    const envid = req.query.envid;
+    console.log(req.query);
     if (!vid) {
-        res.status(400).send("missing ?vid param");
+        res.status(400).send("missing ?vid param (visitor id)");
+    }
+    if (!apikey) {
+        res.status(400).send("missing ?apikey param (apikey)");
+    }
+    if (!envid) {
+        res.status(400).send("missing ?envid param (environment id)");
     }
     let logs = [];
 
     const config = {};
     const fsInstance = Flagship.start(
-        "bvvol5cmicqk8sigckng",
-        "d5UIKzy8Lp58DuASSGmS3dHECjXtpus1mHRtdwe7",
+        envid,
+        apikey,
         config
     );
 
     const fsVisitorInstance = fsInstance.newVisitor(vid, {});
     fsVisitorInstance.on("ready", () => {
         logs.push(JSON.stringify(fsVisitorInstance.fetchedModifications));
-        fsVisitorInstance.fetchedModifications.forEach(function (v, i) {
-            const key = Object.keys(
-            fsVisitorInstance.fetchedModifications[i]["variation"]["modifications"][
-                "value"
-            ]
-            )[0];
-            if (key === "evaluationHourPrice") {
-                logs.push(JSON.stringify(fsVisitorInstance.fetchedModifications[i]["variation"]["modifications"]["value"]));
-            }
-        });
+        if (fsVisitorInstance.fetchedModifications) {
+            fsVisitorInstance.fetchedModifications.forEach(function (v, i) {
+                const key = Object.keys(
+                fsVisitorInstance.fetchedModifications[i]["variation"]["modifications"][
+                    "value"
+                ]
+                )[0];
+                if (key === "evaluationHourPrice") {
+                    logs.push(JSON.stringify(fsVisitorInstance.fetchedModifications[i]["variation"]["modifications"]["value"]));
+                }
+            });
+        }
 
         res.status(200).send(logs.join("<br/><br/>"));
     });
